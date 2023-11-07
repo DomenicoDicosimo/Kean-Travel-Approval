6544d859fd59f4deee757f3ec869e408b18175e9
Zach Gordon <zachgordon25@gmail.com>
2023-11-07 18:53:28 -0500

    Add student travel registration form day model and routes

--- flask-server/server/models.py
+++ flask-server/server/models.py
@@ -41,3 +41,47 @@ class User(db.Model):
 
     def __repr__(self):
         return f"<User {self.fName} {self.lName}>"
+
+
+class StudentTravelRegistrationFormDay(db.Model):
+    __tablename__ = "student_travel_registration_form_day"
+
+    id = db.Column(db.Integer, primary_key=True)
+    event_name = db.Column(db.String(255))
+    host_organization = db.Column(db.String(255))
+    departure_time = db.Column(db.DateTime)
+    approximate_return_time = db.Column(db.DateTime)
+    minimum_age_requirement = db.Column(db.Integer)
+    first_name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
+    kuid = db.Column(db.String(10))
+    email = db.Column(db.String(255))
+    phone_number = db.Column(db.String(15))
+    date_of_birth = db.Column(db.DateTime)
+    current_address = db.Column(db.String(255))
+    city = db.Column(db.String(255))
+    state = db.Column(db.String(2))
+    zip = db.Column(db.String(10))
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "event_name": self.event_name,
+            "host_organization": self.host_organization,
+            "departure_time": str(self.departure_time),
+            "approximate_return_time": str(self.approximate_return_time),
+            "minimum_age_requirement": self.minimum_age_requirement,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "kuid": self.kuid,
+            "email": self.email,
+            "phone_number": self.phone_number,
+            "date_of_birth": str(self.date_of_birth),
+            "current_address": self.current_address,
+            "city": self.city,
+            "state": self.state,
+            "zip": self.zip,
+        }
+
+    def __repr__(self):
+        return f"<StudentTravelRegistrationFormDay(id={self.id}, event_name='{self.event_name}', host_organization='{self.host_organization}', departure_time={self.departure_time}, approximate_return_time={self.approximate_return_time})>"
