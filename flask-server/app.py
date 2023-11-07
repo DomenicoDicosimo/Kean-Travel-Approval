0b93c0f6c6f7ef326ea56d5768707d21a5c1d2dd
DomenicoDicosimo <65661974+DomenicoDicosimo@users.noreply.github.com>
2023-11-07 15:59:45 -0500

    Added, Forms, new route to flask, and database

--- flask-server/app.py
+++ flask-server/app.py
@@ -23,7 +23,89 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
 
 db = SQLAlchemy(app)
 
+class StudentTravelRegistrationFormDay(db.Model):
 
+    __tablename__ = 'student_travel_registration_form_day'
+    id = Column(Integer, primary_key=True)
+    event_name = Column(String(255))
+    host_organization = Column(String(255))
+    departure_time = Column(DateTime)
+    approximate_return_time = Column(DateTime)
+    minimum_age_requirement = Column(Integer)
+    first_name = Column(String(255))
+    last_name = Column(String(255))
+    kuid = Column(String(10))
+    email = Column(String(255))
+    phone_number = Column(String(15))
+    date_of_birth = Column(DateTime)
+    current_address = Column(String(255))
+    city = Column(String(255))
+    state = Column(String(2))
+    zip = Column(String(10))
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'event_name': self.event_name,
+            'host_organization': self.host_organization,
+            'departure_time': str(self.departure_time),  # Convert DateTime to string for JSON serialization
+            'approximate_return_time': str(self.approximate_return_time),
+            'minimum_age_requirement': self.minimum_age_requirement,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'kuid': self.kuid,
+            'email': self.email,
+            'phone_number': self.phone_number,
+            'date_of_birth': str(self.date_of_birth),
+            'current_address': self.current_address,
+            'city': self.city,
+            'state': self.state,
+            'zip': self.zip
+        }
+
+    def __repr__(self):
+        return f"<StudentTravelRegistrationFormDay(id={self.id}, event_name='{self.event_name}', host_organization='{self.host_organization}', departure_time={self.departure_time}, approximate_return_time={self.approximate_return_time})>"
+
+
+@app.route('/studenttravelregistrationformdaysubmit', methods=['POST'])
+def studenttravelregistrationformdaysubmit():
+    data = request.json
+    student_registration_day = StudentTravelRegistrationFormDay(
+         event_name=data.get('event_name'),
+         host_organization=data.get('host_organization'),
+         departure_time=data.get('departure_time'),
+         approximate_return_time=data.get('approximate_return_time'),
+         minimum_age_requirement=data.get('minimum_age_requirement'),
+         first_name=data.get('first_name'),
+         last_name=data.get('last_name'),
+         kuid=data.get('kuid'),
+         email=data.get('email'),
+         phone_number=data.get('phone_number'),
+         date_of_birth=data.get('date_of_birth'),
+         current_address=data.get('current_address'),
+         city=data.get('city'),
+         state=data.get('state'),
+         zip=data.get('zip')
+     )
+
+     db.session.add(student_registration_day)
+     db.session.commit()
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)     
+
+
+
+
+
+
+
+
+'''
 class Staff(db.Model):
     """
     Represents a staff member in the database.
@@ -94,6 +176,4 @@ def test():
 
     return jsonify(message="Hello from Flask")
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
+'''
