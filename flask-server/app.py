99350ac69ee508d3be74523a57ffb64644f9f86a
Zach Gordon <zachgordon25@gmail.com>
2023-11-07 11:52:01 -0500

    Add User model and endpoint to retrieve all users

--- flask-server/app.py
+++ flask-server/app.py
@@ -3,6 +3,7 @@ This module initializes a Flask app and sets up a SQLAlchemy database connection
 """
 import logging
 import os
+from datetime import date
 
 from dotenv import load_dotenv
 from flask import Flask, jsonify
@@ -52,6 +53,54 @@ class Staff(db.Model):
 
     def __repr__(self):
         return f"<Staff {self.name}>"
+
+
+class User(db.Model):
+    __tablename__ = "Users"  # Replace with your actual table name if different
+
+    id = db.Column(db.Integer, primary_key=True)
+    fName = db.Column(db.String(100), nullable=False)
+    lName = db.Column(db.String(100), nullable=False)
+    DOB = db.Column(db.Date, nullable=False)
+    sex = db.Column(db.String(1), nullable=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)
+    street = db.Column(db.String(100), nullable=False)
+    city = db.Column(db.String(50), nullable=False)
+    state = db.Column(db.String(2), nullable=False)
+    zip = db.Column(db.String(10), nullable=False)
+    role = db.Column(db.String(20), nullable=True)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "fName": self.fName,
+            "lName": self.lName,
+            "DOB": self.DOB.strftime("%Y-%m-%d")
+            if isinstance(self.DOB, date)
+            else self.DOB,
+            "sex": self.sex,
+            "email": self.email,
+            "password": self.password,
+            "street": self.street,
+            "city": self.city,
+            "state": self.state,
+            "zip": self.zip,
+            "role": self.role,
+        }
+
+    def __repr__(self):
+        return f"<User {self.fName} {self.lName}>"
+
+
+@app.route("/users")
+def get_all_users():
+    """
+    Returns a JSON representation of all users in the database.
+    """
+
+    users = User.query.all()
+    return jsonify([u.to_dict() for u in users])
 
 
 # testing for all staff
