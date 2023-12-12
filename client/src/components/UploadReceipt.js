686dbcae0b3ab8cae67b5613a5de45d4a56a9b66
Zach Gordon <zachgordon25@gmail.com>
2023-12-12 09:34:07 -0500

    Fixed file upload and display functions

--- client/src/components/UploadReceipt.js
+++ client/src/components/UploadReceipt.js
@@ -57,9 +57,7 @@ const UploadReceipt = () => {
   const renderReceipt = (receipt) => {
     const fileExtension = receipt.file_path.split('.').pop().toLowerCase();
     const filename = receipt.file_path.split('\\').pop();
-    if (['jpg', 'jpeg', 'png'].includes(fileExtension)) {
-      return <img src={receipt.file_path} alt={`Receipt ${receipt.id}`} />;
-    } else if (fileExtension === 'pdf') {
+    if (['jpg', 'jpeg', 'png', 'pdf'].includes(fileExtension)) {
       return (
         <a
           href={`http://127.0.0.1:5000/uploads/${filename}`}
